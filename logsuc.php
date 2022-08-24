021368c7a599f480b113142f4750338e7f20f577
yulsoen <ironbead@naver.com>
2022-08-24 07:30:56 +0000

    logsuc.php

--- logsuc.php
+++ logsuc.php
@@ -7,6 +7,12 @@
     <title>Document</title>
 </head>
 <body>
-    로그인 성공!
+    <h2>로그인 성공!</h2>
+    <hr>
+    <h3> 댓글을 다시는 분은 </h3>
+    <button type="button" onclick="location.href='comment.php'">댓글달기!</button>
+    <br>
+    <h3> 파일 업로드를 원하시는 분은 </h3>
+    <button type="button" onclick="location.href='file_upload.php'">파일등록!</button>
 </body>
 </html>
