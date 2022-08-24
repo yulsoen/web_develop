f8d61230447f3975d8c2d75300cdd3f6e963590e
yulsoen <ironbead@naver.com>
2022-08-24 07:20:24 +0000

    login_action.php

--- login_action.php
+++ login_action.php
@@ -1,19 +1,14 @@
 <?php
+    session_start();
     $login_id = $_POST['login_id'];
     $login_pw = $_POST['lgoin_pw'];
     $con = new mysqli("localhost","yule","zhsks0704","webdev");
-    echo "Tlqkf";
-    $sql = "SELECT * FROM login where login_id='$login_id' and login_pw='$login_pw'";
-    echo "Tlqkf2";
+    $sql = "SELECT * FROM login where login_id= ? and login_pw=?";
     $stmt = $con->prepare($sql);
-    echo "Tlqkf3";
     $stmt->bind_param("ss",$login_id,$login_pw);
-    echo "Tlqkf4";
     $stmt->execute();
-    echo "Tlqkf5";
     $result = $stmt->get_result()->fetch_assoc();
-    echo "Tlqkf6";
-    if($result){
+    if(!$result){
         $_SESSION['login_id'] = $result['login_id'];
         $_SESSION['login_pw'] = $result['login_pw'];
         echo "<script>alert('로그인 성공 :)');";
